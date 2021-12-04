3a92d9d9772480cd2415fd35424e7ae8d7d16f5c
Sanger Steel <sangersteel@Sangers-MacBook-Air.local>
2021-12-04 12:34:02 -0500

    started scraper

--- utils/parse_page.py
+++ utils/parse_page.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 from utils.html_from_url import get_html_from_url
+import numpy as np
+import csv
 
 BASE_PATH = "https://www.presidency.ucsb.edu"
 
@@ -12,17 +14,30 @@ def retrieve_search_page(page_number):
     to_parse = "https://www.presidency.ucsb.edu/documents/app-categories/presidential/spoken-addresses-and-remarks?page=" + str(page_number)
     html = get_html_from_url(to_parse)
     soup = BeautifulSoup(html, 'html.parser')
-    return soup
+    return soup.find_all('div', {"class":"node node-documents node-teaser view-mode-teaser"})
 
 def get_author_and_transcript_urls(soup, result_no):
-    results = soup.find_all('div', {"class":"node node-documents node-teaser view-mode-teaser"})[result_no].find_all('a')
+    results = soup[result_no].find_all('a')
+    transcript_title = results[0].text
     link = BASE_PATH + results[0]['href']
     author = results[1].text
-    return link, author
+    return link, author, transcript_title
 
 
 def retrieve_transcript(transcript_url):
     soup = view_page(transcript_url)
     return soup.find_all('div', {"class":"field-docs-content"})[0].find_all('p')
+
+
+def retrieve_data(max_pages, start_page):
+    with open('results.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        for i in np.arange(start_page, max_pages, 1):
+            search_page = retrieve_search_page(i)
+            for j in range(len(search_page)):
+                link, author, transcript_title = get_author_and_transcript_urls(search_page, j)
+                transcript = retrieve_transcript(link)
+                writer.writerow([i,j, author, transcript_title, link, transcript])
+        return 0
 
 
